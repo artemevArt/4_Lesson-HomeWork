40bfd7c374e4c05a61cc988cd4d32727a227aefc
Artem Artemev <artem@artemev.su>
2022-12-24 16:30:52 +0300

    1st commented out

--- Program.cs
+++ Program.cs
@@ -1,42 +1,42 @@
-﻿int RasingPositiveDegree(int num1, int num2)
-{
-    int result = 1;
-    for (int i = 0; i < num2; i++)
-    {
-        result = result * num1;
-    }
-    return result;
-}
+﻿// int RasingPositiveDegree(int num1, int num2)
+// {
+//     int result = 1;
+//     for (int i = 0; i < num2; i++)
+//     {
+//         result = result * num1;
+//     }
+//     return result;
+// }
 
-double RasingNegativeDegree(int num1, int num2)
-{
-    double result = 1;
-    num2 = num2 * (-1);
-    for (int i = 0; i < num2; i++)
-    {
-        result = result * num1;
-    }
-    result = 1 / result;
-    return result;
-}
+// double RasingNegativeDegree(int num1, int num2)
+// {
+//     double result = 1;
+//     num2 = num2 * (-1);
+//     for (int i = 0; i < num2; i++)
+//     {
+//         result = result * num1;
+//     }
+//     result = 1 / result;
+//     return result;
+// }
 
-int a, b;
+// int a, b;
 
-Console.WriteLine("Введите число: ");
-a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("В какую степень необходимо возвести число " + a + ":");
-b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("В какую степень необходимо возвести число " + a + ":");
+// b = Convert.ToInt32(Console.ReadLine());
 
 
-if (b == 0)
-{
-    Console.WriteLine("Любое число возведенное в 0 степень будет равняться 1!");
-}
-else if (b < 0)
-{
-    Console.WriteLine($"Число {a} в {b} степени = {RasingNegativeDegree(a, b)}");
-}
-else
-    if (b == 1) Console.WriteLine($"Число {a} в {b} степени = {a}");
-else
-    Console.WriteLine($"Число {a} в {b} степени = {RasingPositiveDegree(a, b)}");
+// if (b == 0)
+// {
+//     Console.WriteLine("Любое число возведенное в 0 степень будет равняться 1!");
+// }
+// else if (b < 0)
+// {
+//     Console.WriteLine($"Число {a} в {b} степени = {RasingNegativeDegree(a, b)}");
+// }
+// else
+//     if (b == 1) Console.WriteLine($"Число {a} в {b} степени = {a}");
+// else
+//     Console.WriteLine($"Число {a} в {b} степени = {RasingPositiveDegree(a, b)}");
